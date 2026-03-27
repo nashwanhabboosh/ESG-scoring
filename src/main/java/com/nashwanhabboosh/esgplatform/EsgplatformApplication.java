9f1d3460f90f05a2d570b69a03c96a1a95c4123d
nashwanhabboosh <nashwan.habboosh@icloud.com>
2026-03-27 16:00:50 -0400

    Setup Springboot

---
+++ src/main/java/com/nashwanhabboosh/esgplatform/EsgplatformApplication.java
@@ -0,0 +1,13 @@
+package com.nashwanhabboosh.esgplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsgplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsgplatformApplication.class, args);
+	}
+
+}
